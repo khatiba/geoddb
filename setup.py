e86c80a51c0e14d7be9d2ed9c148b5b0bcdd139c
Ahmad Khatib <ackhatib@gmail.com>
2022-01-07 09:41:11 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='geoddb',
-    version='0.1.0',
+    version='0.1.1',
     scripts=[] ,
     author='Ahmad Khatib',
     author_email='ackhatib@gmail.com',
